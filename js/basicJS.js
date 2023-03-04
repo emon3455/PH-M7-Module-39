dd36504aafc8b1bbf0c4df50c68c360c9beabfad
emon3455 <emon.mhk69@gmail.com>
2023-03-05 00:00:01 +0600

    v8 engine

--- js/basicJS.js
+++ js/basicJS.js
@@ -2,3 +2,10 @@
 /*
 javascript is just in time a highly abstract dynamically prototyped programming language.
 */
+
+// what is v8 engine ?
+/*
+If we apply js code in chrome there is an engine called v8 engine which executes the code.
+
+v8 is a high performance javascript and webAssable engine.
+*/
